b7e751f8b7c576a3ab16288f7958b6f40dc03e3c
Daniel Primo <delineas@gmail.com>
2020-03-25 11:33:54 +0100

    Update AboutModal.vue

--- src/components/AboutModal.vue
+++ src/components/AboutModal.vue
@@ -8,7 +8,7 @@
             <p>Es una aplicación web para colocar recursos geolocalizados en un mapa.</p>
             <p>La idea es crear una aplicación de este tipo por demanda o especialidad.</p>
             <p>Se puede publicar gratuitamente en la nube.</p>
-            <p>Más info en <a href="https://github.com/delineas/help-maps-party">está página</a></p>
+            <p>El origen de todo esto lo cuento <a href="https://www.danielprimo.io/blog/storytelling-para-crear-una-aplicacion-web">en está página</a></p>
             <br />
           </div>
         </div>
